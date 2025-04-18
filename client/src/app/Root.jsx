bed0ffe06dc86f3fdd215c99f7262ebb2af82481
Nikita230100 <rozhnev231@gmail.com>
2025-04-18 12:55:54 +0500

    initial

---
+++ client/src/app/Root.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Outlet } from 'react-router';
+import Header from '../widgets/Header/Header';
+import Footer from '../widgets/Footer/Footer';
+
+
+export default function Root() {
+  return (
+    <div className='app'>
+       <Header />
+      <main className='main'>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
